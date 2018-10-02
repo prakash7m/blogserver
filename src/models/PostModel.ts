69355d9d328ffd82b9c2e1351fa2315f18548903
AUSTRALIA\Prakash.Paudel <prakash.paudel@civica.com.au>
2018-10-02 16:38:50 +1000

    added posts

---
+++ src/models/PostModel.ts
@@ -0,0 +1,71 @@
+import bcrypt from 'bcrypt';
+import { Schema, model, Document, Model } from 'mongoose';
+
+/**
+ * Post model interface required for post update, where ID will be the finder.
+ *
+ * @export
+ * @interface IPostModel
+ * @extends {Document}
+ */
+export interface IPostModel extends Document {
+  /**
+   * Post's title
+   *
+   * @type {string}
+   * @memberof IPostUpdate
+   */
+  title: string;
+  /**
+   * Post's synopsis
+   *
+   * @type {string}
+   * @memberof IPostUpdate
+   */
+  synopsis: string;
+  slug: string;
+  content?: string;
+  hero?: string;
+  category?: string[];
+  active?: boolean;
+  user?: string;
+  created?: string;
+  updated?: string;
+  meta?: string;
+  stars?: number;
+  readtime?: number;
+  views?: number;
+};
+
+export const PostSchema: Schema = new Schema({
+  title: { type: String, required: true, trim: true },
+  synopsis: { type: String, trim: true },
+  slug: { type: String, required: true, unique: true, trim: true },
+  content: { type: String, trim: true },
+  hero: { type: Schema.Types.ObjectId, ref: 'Media' },
+  category: [{ type: Schema.Types.ObjectId, ref: 'Category', required: true }],
+  active: { type: Boolean },
+  user: { type: Schema.Types.ObjectId, ref: 'User' },  
+  created: { type: Date, required: true, default: Date.now },
+  updated: { type: Date, required: true, default: Date.now },
+  meta: { type: String, trim: true },
+  stars: { type: Number },
+  readtime: { type: Number },
+  views: { type: Number }
+});
+
+PostSchema.pre('save', async function (this: any, next) {
+  try {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+PostSchema.methods.validatePassword = async function (password: string): Promise<boolean> {
+  return await bcrypt.compare(password, this.password);
+}
+
+export const Post: Model<IPostModel> = model<IPostModel>('Post', PostSchema);
---
+++ src/validators/PostValidator.ts
@@ -0,0 +1,92 @@
+import { check } from 'express-validator/check';
+import { RequestHandler } from 'express';
+import { sanitizeParam } from 'express-validator/filter';
+
+import { BaseValidationMiddleware } from "./BaseValidationMiddleware";
+import { Post } from "../models/PostModel";
+
+
+/**
+ * Post operation validator class. Has different static methods to validate fetch, list, create, update and remove
+ *
+ * @export
+ * @class PostValidationMiddleware
+ * @extends {BaseValidationMiddleware}
+ */
+export class PostValidationMiddleware extends BaseValidationMiddleware {
+  /**
+   * Validates if post exists before proceeding.
+   *
+   * @static
+   * @returns {RequestHandler[]}
+   * @memberof PostValidationMiddleware
+   */
+  public static fetch(): RequestHandler[] {
+    return [
+      super.checkIdExists(Post, 'Post not found.'),
+      super.sendErrors()
+    ];
+  }
+
+  /**
+   * Validates the page parameter.
+   * Tries to cast the page to int if any.
+   * If invalid page parameter (optional), it prevents proceeding.
+   *
+   * @static
+   * @returns {RequestHandler[]}
+   * @memberof PostValidationMiddleware
+   */
+  public static list(): RequestHandler[] {
+    return [
+      sanitizeParam('page').toInt(),
+      check('page').optional().isInt().withMessage("Invalid page parameter"),
+      super.sendErrors()
+    ];
+  }
+
+  /**
+   * Validates the email, postname and password before creating post.
+   *
+   * @static
+   * @returns {RequestHandler[]}
+   * @memberof PostValidationMiddleware
+   */
+  public static create(): RequestHandler[] {
+    return [
+      check('title').isLength({ min: 5 }).withMessage("Should be min 5 characters"),      
+      check('slug').isLength({ min: 5 }).withMessage("Should be min 5 characters"),
+      super.sendErrors()
+    ];
+  }
+
+  /**
+   * Checks if post exists, and if yes, validates the update parameters before proceeding.
+   *
+   * @static
+   * @returns {RequestHandler[]}
+   * @memberof PostValidationMiddleware
+   */
+  public static update(): RequestHandler[] {
+    return [
+      super.checkIdExists(Post, 'Post not found.'),
+      check('title').isLength({ min: 5 }).withMessage("Should be min 5 characters"),      
+      check('slug').isLength({ min: 5 }).withMessage("Should be min 5 characters"),
+      super.sendErrors()
+    ];
+  }
+
+  /**
+   * Checks if post exists before removing.
+   *
+   * @static
+   * @returns {RequestHandler[]}
+   * @memberof PostValidationMiddleware
+   */
+  public static remove(): RequestHandler[] {
+    return [
+      super.checkIdExists(Post, 'Post not found.'),
+      super.sendErrors()
+    ];
+  }
+}
